1d046bd3508c76b02654df20dd1dab08437ad6e0
Hossam Samir <Hossameos1@gmail.com>
2018-01-18 19:10:00 +0200

    removing garbage

--- server/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-exports.sayHello = () => {
-	app.get('/', (req, res) => res.render('index'));
-	app.get('/home', (req, res) => res.render('home'));
-	console.log('asfasfasfasf');
-};
