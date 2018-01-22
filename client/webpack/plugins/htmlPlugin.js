9b9d769fbba15a381291cddf5c5d53ab59b709e6
Khalid <khalidahmed333@hotmail.com>
2018-01-22 18:31:40 +0200

    store owner dashboard almost done

--- client/webpack/plugins/htmlPlugin.js
+++ client/webpack/plugins/htmlPlugin.js
@@ -1,28 +1,30 @@
-const
-  path              = require('path'),
-  manifest          = require('../manifest'),
+const path = require('path'),
+  manifest = require('../manifest'),
   HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const titles = {
-  'index': 'Dashboard',
-  'buttons': 'Buttons',
-  'calendar': 'Calendar',
-  'charts': 'Charts',
-  'chat': 'Chat',
-  'compose': 'Compose',
-  'datatable': 'Datatable',
-  'markets': 'markets',
-  'users': 'users',
-  'email': 'Email',
-  'forms': 'Forms',
+  index: 'Dashboard',
+  buttons: 'Buttons',
+  calendar: 'Calendar',
+  products: 'products',
+  orders: 'orders',
+  offers: 'offers',
+  charts: 'Charts',
+  chat: 'Chat',
+  compose: 'Compose',
+  datatable: 'Datatable',
+  markets: 'markets',
+  users: 'users',
+  email: 'Email',
+  forms: 'Forms',
   'google-maps': 'Google Maps',
-  'signin': 'Signin',
-  'signup': 'Signup',
-  'ui': 'UI',
+  signin: 'Signin',
+  signup: 'Signup',
+  ui: 'UI',
   'vector-maps': 'Vector Maps',
   '404': '404',
   '500': '500',
-  'basic-table': 'Basic Table',
+  'basic-table': 'Basic Table'
 };
 
 module.exports = Object.keys(titles).map(title => {
@@ -36,7 +38,7 @@ module.exports = Object.keys(titles).map(title => {
       minifyCSS: true,
       minifyJS: true,
       removeComments: true,
-      useShortDoctype: true,
-    },
+      useShortDoctype: true
+    }
   });
 });
