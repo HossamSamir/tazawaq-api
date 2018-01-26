8f0ad87ddf7e2ac576933f68e6713dfddadf52c9
Khalid <khalidahmed333@hotmail.com>
2018-01-26 17:17:51 +0200

    Database completely established

--- database/tazawaq.sql
+++ database/tazawaq.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 22, 2018 at 11:30 PM
+-- Generation Time: Jan 26, 2018 at 04:17 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `tazawaq`
 --
-CREATE DATABASE IF NOT EXISTS `tazawaq` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `tazawaq` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `tazawaq`;
 
 -- --------------------------------------------------------
@@ -35,12 +35,146 @@ CREATE TABLE `awaiting_verification` (
   `time_generated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `code` int(11) NOT NULL,
   `phone` varchar(16) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(64) NOT NULL,
   `password` varchar(64) NOT NULL,
   `location` varchar(128) NOT NULL,
   `latitude` double NOT NULL,
   `longitude` double NOT NULL,
   `region` varchar(64) NOT NULL,
   `country` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `store_id` int(11) NOT NULL,
+  `name` varchar(32) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expo_push_tokens`
+--
+
+CREATE TABLE `expo_push_tokens` (
+  `id` int(11) NOT NULL,
+  `token` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offers`
+--
+
+CREATE TABLE `offers` (
+  `id` int(11) NOT NULL,
+  `store_id` int(11) NOT NULL,
+  `name` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `info` varchar(512) CHARACTER SET utf8 NOT NULL,
+  `cost_before` decimal(8,3) NOT NULL,
+  `cost_after` decimal(8,3) NOT NULL,
+  `status` int(11) NOT NULL,
+  `img` varchar(256) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `store_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cost` decimal(8,3) NOT NULL,
+  `info` varchar(512) CHARACTER SET utf8 NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `store_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `info` varchar(512) CHARACTER SET utf8 NOT NULL,
+  `cost` decimal(8,3) NOT NULL,
+  `status` int(11) NOT NULL,
+  `img` varchar(256) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL,
+  `store_id` int(11) NOT NULL,
+  `info` varchar(512) CHARACTER SET utf8 NOT NULL,
+  `location` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `cost` decimal(8,3) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stores`
+--
+
+CREATE TABLE `stores` (
+  `id` int(11) NOT NULL,
+  `passname` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `display_name` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `address` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `country` varchar(80) CHARACTER SET utf8 NOT NULL,
+  `region` varchar(64) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket_messages`
+--
+
+CREATE TABLE `ticket_messages` (
+  `id` int(11) NOT NULL,
+  `ticket_id` int(11) NOT NULL,
+  `sender` int(11) NOT NULL,
+  `message` varchar(512) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `id` int(11) NOT NULL,
+  `store_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -51,13 +185,15 @@ CREATE TABLE `awaiting_verification` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `phone` varchar(16) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `location` varchar(128) NOT NULL,
+  `phone` varchar(16) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `username` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `location` varchar(128) CHARACTER SET utf8 NOT NULL,
   `latitude` double NOT NULL,
   `longitude` double NOT NULL,
-  `region` varchar(64) NOT NULL,
-  `country` varchar(80) NOT NULL
+  `region` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `country` varchar(80) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -68,6 +204,60 @@ CREATE TABLE `users` (
 -- Indexes for table `awaiting_verification`
 --
 ALTER TABLE `awaiting_verification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `expo_push_tokens`
+--
+ALTER TABLE `expo_push_tokens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `offers`
+--
+ALTER TABLE `offers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stores`
+--
+ALTER TABLE `stores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ticket_messages`
+--
+ALTER TABLE `ticket_messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tickets`
+--
+ALTER TABLE `tickets`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -84,6 +274,51 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `awaiting_verification`
 --
 ALTER TABLE `awaiting_verification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `expo_push_tokens`
+--
+ALTER TABLE `expo_push_tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `offers`
+--
+ALTER TABLE `offers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `stores`
+--
+ALTER TABLE `stores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ticket_messages`
+--
+ALTER TABLE `ticket_messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tickets`
+--
+ALTER TABLE `tickets`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
