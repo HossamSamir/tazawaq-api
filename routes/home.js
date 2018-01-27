dba09e242af6d557a53622c846b6d45ca1fa8638
Khalid <khalidahmed333@hotmail.com>
2018-01-27 19:19:26 +0200

    Working on admin's homepage, addresses #2

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,20 @@
+function qry(query, callback) {
+	con.query(query, function(err,result) {
+		if(err) res.json(err);
+		else callback(result);
+	});
+}
+
 function travers(req, res) {
-	res.render('index');
+
+	qry('SELECT COUNT(id) AS cnt FROM users', function(usersCount) {
+		qry('SELECT COUNT(id) AS cnt FROM stores', function(storesCount) {
+			qry('SELECT COUNT(id) AS cnt FROM sales', function(salesCount) {
+				res.render('index', { usersCt: usersCount[0].cnt,
+					storesCt: storesCount[0].cnt, salesCt: salesCount[0].cnt });
+			});
+		});
+	});
 }
 
 module.exports = travers;
