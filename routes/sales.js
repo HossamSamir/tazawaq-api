d0ef3c013bfcd5a8072fcac049a5097e1fa717a0
Khalid <khalidahmed333@hotmail.com>
2018-01-28 16:40:53 +0200

    Admin: sales page, closes #3

--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,16 @@
 function travers(req, res) {
-	res.render('sales');
+	sql.qry('SELECT cost,date,store_id,info,location FROM sales ORDER BY id DESC LIMIT 5', function(sales_res) {
+		var sales = [];
+		async.forEachOf(sales_res, function (sale, i, callback) {
+			sql.qry('SELECT display_name FROM stores WHERE id=? LIMIT 1', [ sale.store_id ], function(storeName) {
+				sales.push( { name: storeName[0].display_name, data: sale } );
+				callback(null);
+			});
+		}, function(err) {
+			if(err) throw err;
+			res.render('sales', { sales	});
+		});
+	});
 }
 
 module.exports = travers;
