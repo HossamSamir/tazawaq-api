e047c7d6cbfb308f4b9b3a4bf8294769904e62db
Khalid <khalidahmed333@hotmail.com>
2018-01-28 14:15:55 +0200

    Homepage, closes #2

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,25 @@ function travers(req, res) {
 	sql.qry('SELECT COUNT(id) AS cnt FROM users', function(usersCount) {
 		sql.qry('SELECT COUNT(id) AS cnt FROM stores', function(storesCount) {
 			sql.qry('SELECT COUNT(id) AS cnt FROM sales', function(salesCount) {
-				res.render('index', { usersCt: usersCount[0].cnt,
-					storesCt: storesCount[0].cnt, salesCt: salesCount[0].cnt });
+				sql.qry('SELECT SUM(cost) AS totalCost FROM sales', function(salesCst) {
+					sql.qry('SELECT cost,date,store_id,info FROM sales ORDER BY id DESC LIMIT 5', function(sales_res) {
+						var sales = [];
+						for(var i = 0; i != sales_res.length; ++ i)
+						{
+							sql.qry('SELECT display_name FROM stores WHERE id=?', [ sales_res[i].store_id ], function(storesNames) {
+								sales.push({ name: storesNames[i].name, data: sales_res[i] });
+							});
+						}
+						sql.qry('SELECT display_name,passname,region FROM stores ORDER BY id DESC LIMIT 4', function(stores) {
+							res.render('index', { usersCt: usersCount[0].cnt,
+								storesCt: storesCount[0].cnt,
+								salesCt: salesCount[0].cnt,
+								salesCost: salesCst[0].totalCost,
+								sales, stores
+						 	});
+						});
+					});
+				});
 			});
 		});
 	});
