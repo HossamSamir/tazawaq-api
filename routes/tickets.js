87a3b565be1140ce72d10642932338428f99610b
Khalid <khalidahmed333@hotmail.com>
2018-01-31 20:45:48 +0200

    Fetch and display tickets and their messages, closes #25

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,5 +1,15 @@
 function travers(req, res) {
-	res.render('tickets');
+	sql.qry('SELECT id, title, status FROM tickets ORDER BY status DESC, id DESC', function(tickets) {
+    	res.render('tickets', { tickets });
+    });
 }
 
 module.exports = travers;
+
+app.get('/fetch-ticket-messages',function(req,res){
+    var ticket_id = req.param("id");
+
+    sql.qry('SELECT message,sender_type FROM ticket_messages WHERE ticket_id=? ORDER BY id ASC', [ticket_id],function(messages) {
+        res.json({ messages });
+    });
+});
