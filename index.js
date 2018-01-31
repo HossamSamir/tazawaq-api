579bccf1c4e64347ca0e13f23127858f5241ccba
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-01-31 04:07:45 +0200

    make it better

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (typeof localStorage === 'undefined' || localStorage === null) {
 var mysql = require('mysql');
 
 con = mysql.createConnection({
-	host: 'fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+	host: 'fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
 	user: 'yv7zhu2a0knkxik6',
 	password: 'ke9vo85qqgvps1n6',
 	database: 't8chpprig1nsyy5t'
