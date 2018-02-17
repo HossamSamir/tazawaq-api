cfd4d989bb0e6d38b4deedd38323508729dffdd3
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-02-17 21:55:54 +0200

    lll

--- routes/api/meals_by_ids.js
+++ routes/api/meals_by_ids.js
@@ -6,23 +6,26 @@ app.get('/api/meals-by-ids',function(req,res){
     var meals = [];
     for(let i in ids){
       if(ids[i] != null || ids[i] != 'null'){
-      con.query('SELECT id AS `key`, name, img AS image, cost AS price, info AS `desc` '+
-           'FROM products WHERE id=? LIMIT 1', [ids[i]], function(err,data) {
-          if(!err) {
-            var data = data[0];
-            meals.push(data)
-          }
-          else {
-              res.json({ response:0, err });
-          }
-          if(i == ids.length-1){
-            console.log(meals)
-            res.json({
-                 meals
-            });
-          }
-      });
-    }
+        con.query('SELECT id AS `key`, name, img AS image, cost AS price, info AS `desc` '+
+             'FROM products WHERE id=? LIMIT 1', [ids[i]], function(err,data) {
+            if(!err) {
+              var data = data[0];
+              if(data != null){
+                meals.push(data)
+              }
+
+            }
+            else {
+                res.json({ response:0, err });
+            }
+            if(i == ids.length-1){
+              console.log(meals)
+              res.json({
+                   meals
+              });
+            }
+        });
+      }
     }
 
 });
