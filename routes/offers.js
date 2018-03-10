caaa0d465ee6ccdad8a83ea0b613c58e5ae82b69
Khalid <khalidahmed333@hotmail.com>
2018-03-10 02:45:06 +0200

    Addendum to last commit

--- routes/offers.js
+++ routes/offers.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function travers(req, res) {
 	sql.qry(
-		'SELECT id,store_id, name, info, cost_before, cost_after, status, img FROM offers',
+		'SELECT id,store_id, name, info, status, img FROM offers',
 		offers => {
 			sql.qry('SELECT id, display_name from stores', stores => {
 				offers.forEach(offer => {
@@ -21,8 +21,6 @@ function travers(req, res) {
 
 app.post('/add-offer', function(req, res) {
 	var name = req.param('name');
-	var cost_after = req.param('cost_after');
-	var cost_before = req.param('cost_before');
 	var store_id = req.param('store_id');
 	var info = req.param('info');
 	var status = req.param('status');
@@ -32,15 +30,13 @@ app.post('/add-offer', function(req, res) {
 		image === null ||
 		!info ||
 		!status ||
-		!cost_after ||
-		!cost_before ||
 		!store_id
 	) {
 		res.send('هناك مدخلات ناقصة او لم تُكتب بشكل صحيح من فضلك راجعها');
 	} else {
 		sql.qry(
-			'INSERT INTO offers (name, cost_after, cost_before, info, status, store_id, img) VALUES(?,?,?,?,?,?,"")',
-			[name, cost_after, cost_before, info, status, store_id, image],
+			'INSERT INTO offers (name, info, status, store_id, img) VALUES(?,?,?,?,"")',
+			[name, info, status, store_id, image],
 			function(response) {
 				var img_path = `client/views/assets/static/images/uploaded_images/offers_images/offer_${
 					response.insertId
