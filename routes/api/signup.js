ea2e713ee0d25e55089cd69f730fc839eafa5e0f
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-04-15 00:44:23 +0200

    aaa

--- routes/api/signup.js
+++ routes/api/signup.js
@@ -2,8 +2,8 @@ var crypto = require('crypto');
 function SendCode(to, code) {
     const fetch = require('node-fetch');
 
-    var msg = 'Tathouq | your verification code is: ' + code;
-    fetch('https://api.smsglobal.com/http-api.php?action=sendsms&user=f2yulf7m&password=2DiKaVq9&&from=tathouq&to=966'+
+    var msg = 'هلا وغلا بك في تطبيق طلباتك : رمز التحقق الخاص بك   ' + code;
+    fetch('https://api.smsglobal.com/http-api.php?action=sendsms&user=f2yulf7m&password=2DiKaVq9&&from=+966503212099&to=966'+
     to+'&text='+msg+'',
     {
         method: 'POST',
