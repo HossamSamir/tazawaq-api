a03f6d1480854820b6048de13d626660c478c41d
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-04-28 23:04:00 +0200

    aaa

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 
 // Config connection
 con = mysql.createConnection({
-	host: 'fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-	user: 'yv7zhu2a0knkxik6',
-	password: 'ke9vo85qqgvps1n6',
-	database: 't8chpprig1nsyy5t'
+	host: '159.89.95.28',
+	user: 'amr',
+	password: 'a33304454',
+	database: 'tazawq'
 });
