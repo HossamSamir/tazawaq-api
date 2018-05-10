ca03400c2ce18d7f69b78914960febf3cd4ef5db
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-05-11 01:33:06 +0200

    reports update

--- routes/sales.js
+++ routes/sales.js
@@ -1,17 +1,55 @@
 function travers(req, res) {
-	sql.qry('SELECT cost,date,store_id,info,location FROM sales ORDER BY id DESC', function(sales_res) {
-		var sales = [];
-		async.forEachOf(sales_res, function (sale, i, callback) {
-			sql.qry('SELECT display_name FROM stores WHERE id=? LIMIT 1', [ sale.store_id ], function(storeName) {
-				if(storeName.length) sales.push( { name: storeName[0].display_name, data: sale } );
-				else sales.push( { name: 'غير متوفر الآن', data: sale } );
-				callback(null);
-			});
-		}, function(err) {
-			if(err) throw err;
-			res.render('sales', { sales	});
-		});
-	});
+	sql.qry(
+		'SELECT id,cost,delivery_cost,info,location,user_id,status,store_id,TIMESTAMPDIFF(MINUTE,time_ordered,NOW()) AS elapsed_time,TIMESTAMPDIFF(MINUTE,time_accepted,NOW()) AS accepted_time_passed,note FROM orders where status=2 ORDER BY id DESC',
+		function(orders_res) {
+			var orders = [];
+			async.forEachOf(
+				orders_res,
+				function(order, i, callback) {
+					sql.qry(
+						'SELECT username, phone FROM users WHERE id=? LIMIT 1',
+						[order.user_id],
+						function(userData) {
+							sql.qry(
+								'SELECT display_name, passname FROM stores WHERE id=? LIMIT 1',
+								[order.store_id],
+								function(storeData) {
+
+									if (userData.length) orders.push({
+											username: userData[0].username,
+											phone: userData[0].phone,
+											data: order,
+											store_display_name: storeData[0].display_name,
+											store_passname: storeData[0].passname
+										});
+									else orders.push({ username: 'غير متاح', phone: '', data: order, store_display_name: storeData[0].display_name,
+										store_passname: storeData[0].passname });
+
+									callback(null);
+							});
+						}
+					);
+				},
+				function(err) {
+					if (err) throw err;
+					res.render('sales', { sales:orders });
+				}
+			);
+		}
+	);
+	// sql.qry('SELECT cost,date,store_id,info,location FROM sales ORDER BY id DESC', function(sales_res) {
+	// 	var sales = [];
+	// 	async.forEachOf(sales_res, function (sale, i, callback) {
+	// 		sql.qry('SELECT display_name FROM stores WHERE id=? LIMIT 1', [ sale.store_id ], function(storeName) {
+	// 			if(storeName.length) sales.push( { name: storeName[0].display_name, data: sale } );
+	// 			else sales.push( { name: 'غير متوفر الآن', data: sale } );
+	// 			callback(null);
+	// 		});
+	// 	}, function(err) {
+	// 		if(err) throw err;
+	// 		res.render('sales', { sales	});
+	// 	});
+	// });
 }
 
 module.exports = travers;
--- routes/store_owner/sales.js
+++ routes/store_owner/sales.js
@@ -1,8 +1,40 @@
 function travers(req, res) {
 	var store_id = req.params.store_id;
-	sql.qry('SELECT cost,date,info,location FROM sales WHERE store_id=? ORDER BY id DESC', [store_id], function(sales) {
-		res.render('store_owner/sales', { sales, store_id });
-	});
+	sql.qry(
+		'SELECT id,cost,delivery_cost,info,location,user_id,status,TIMESTAMPDIFF(MINUTE,time_ordered,NOW()) AS elapsed_time,TIMESTAMPDIFF(MINUTE,time_accepted,NOW()) AS accepted_time_passed,note FROM orders WHERE store_id=? and status=2 ORDER BY id DESC',
+		[store_id],
+		function(orders_res) {
+			var orders = [];
+			async.forEachOf(
+				orders_res,
+				function(order, i, callback) {
+					sql.qry(
+						'SELECT username, phone FROM users WHERE id=? LIMIT 1',
+						[order.user_id],
+						function(userData) {
+							if (userData.length)
+								orders.push({
+									username: userData[0].username,
+									phone: userData[0].phone,
+									data: order
+								});
+							else
+								orders.push({ username: 'غير متاح', phone: '', data: order });
+							callback(null);
+						}
+					);
+				},
+				function(err) {
+					if (err) throw err;
+					res.render('store_owner/sales', { store_id, sales:orders });
+				}
+			);
+		}
+	);
+	// var store_id = req.params.store_id;
+	// sql.qry('SELECT cost,date,info,location FROM sales WHERE store_id=? ORDER BY id DESC', [store_id], function(sales) {
+	// 	res.render('store_owner/sales', { sales, store_id });
+	// });
 }
 
 module.exports = travers;
