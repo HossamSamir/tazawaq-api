9938fdbcb5acf2471562e6f2a967d09f793ac7eb
Amr Mohamed <amr2010mohamd2010@gmail.com>
2018-06-21 14:16:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 // Config connection
 con = mysql.createConnection({
 	host: '132.148.244.83',
-	user: 'talbatk',
+	user: 'root',
 	password: 'A33304454a@',
 	database: 'cphulkd'
 });
