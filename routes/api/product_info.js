8a7ba71fec161e3a17dd06feb8f220ffdda2298b
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-07-18 22:02:09 +0200

    aaa

--- routes/api/product_info.js
+++ routes/api/product_info.js
@@ -4,15 +4,20 @@ app.get('/api/product-info',function(req,res){
     var product_id = req.param("product_id");
 
     con.query('SELECT id AS `key`, name, img AS image, cost AS price, info AS `desc` '+
-         'FROM products WHERE id=? LIMIT 1', [product_id], function(err,data) {
+         'FROM products WHERE id=? and parent_id = 0 LIMIT 1', [product_id], function(err,data) {
         if(!err) {
             if(data.length == 0) return res.json({ response: 0 });
             else
             {
+              con.query('SELECT id AS `key`, name, img AS image, cost AS price, info AS `desc` '+
+                   'FROM products WHERE parent_id = ? ', [product_id], function(err,data2) {
+
                 res.json({
-                   
-                    response: data[0]
+
+                    response: data[0],
+                    childs:data2
                 });
+              });
             }
         }
         else {
