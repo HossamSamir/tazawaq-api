741912dcd3b8aaddeaedc5608e52d0173b81f26a
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-08-14 16:56:32 +0200

    aaa

--- config.js
+++ config.js
@@ -2,17 +2,20 @@
 var mysql = require('mysql');
 
 // Config connection
-/*
+
 con = mysql.createConnection({
 		host: 'localhost',
         user: 'cphulkd',
         password: 'A33304454a@',
         database: 'cphulkd'
 });
-*/
+
+
+/*
 con = mysql.createConnection({
 		host: 'localhost',
         user: 'root',
         password: '',
         database: 'talbatk'
 });
+*/
