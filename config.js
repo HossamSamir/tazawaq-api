a62522dd7b16583f92301e2d57d3f77c9169cb83
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-08-29 23:56:44 +0200

    lll

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ var mysql = require('mysql');
 // Config connection
 
 con = mysql.createConnection({
-		host: 'localhost',
-        user: 'cphulkd',
-        password: 'A33304454a@',
-        database: 'cphulkd'
+		host: '66.45.240.101',
+        user: 'admin',
+        password: 'amr',
+        database: 'talbatk'
 });
 
 
